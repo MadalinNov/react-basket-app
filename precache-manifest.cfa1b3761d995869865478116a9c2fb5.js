bfd1d868d4ba449453d84c059d90affc4810696d
unknown <madalinnov@gmail.com>
2020-10-21 09:32:43 +0300

    another commit

---
+++ precache-manifest.cfa1b3761d995869865478116a9c2fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8c54d61c7126f89da677f8472a7494",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c5c91884d48bd016c88",
+    "url": "./static/css/main.98b89c66.chunk.css"
+  },
+  {
+    "revision": "b902e077ffba56447de7",
+    "url": "./static/js/2.5c8f6f7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5c8f6f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5c91884d48bd016c88",
+    "url": "./static/js/main.b2631ca4.chunk.js"
+  },
+  {
+    "revision": "930e1c544247c7610b02",
+    "url": "./static/js/runtime-main.e751cd40.js"
+  }
+]);
