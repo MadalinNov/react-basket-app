3e223ff69a3576eb7a33550d116385fd9f2c64b4
unknown <madalinnov@gmail.com>
2020-10-21 09:21:02 +0300

    build 0.1.0

---
+++ precache-manifest.31ba2692f8f72c387f5530540bb8e731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2dc1fb03916aa89decb04654eee394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c5c91884d48bd016c88",
+    "url": "/static/css/main.98b89c66.chunk.css"
+  },
+  {
+    "revision": "b902e077ffba56447de7",
+    "url": "/static/js/2.5c8f6f7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c8f6f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5c91884d48bd016c88",
+    "url": "/static/js/main.b2631ca4.chunk.js"
+  },
+  {
+    "revision": "b42c10ddb0d2d1515f54",
+    "url": "/static/js/runtime-main.80328c6b.js"
+  }
+]);
